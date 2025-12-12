502555b702c62ae268d2412aa7cc4cb8c923dd3b
Sroemkhu <Sroemkhunnn@gmail.com>
2025-12-12 17:14:35 +0700

    Rename lab8_3.cpp to lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+if(x == 'A') return 'Z';
+    if(x >= 'B' && x <= 'Z') return x - 1;
+    return '0';
 }
 
 int main(){
@@ -11,7 +13,7 @@ int main(){
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
 	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
+  	cout << before('Z') << "\n";
 	cout << before('a') << "\n";
 	cout << before('0') << "\n";
 	cout << before('c') << "\n";
